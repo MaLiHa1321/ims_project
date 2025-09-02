af5b8bd2c113afdac476687d315da1d2dad29ffd
Bushrakhan15 <ummehabiba.bushra2000@gmail.com>
2025-09-02 20:38:48 +0600

    get element are added

--- backend/server.js
+++ backend/server.js
@@ -25,14 +25,18 @@ app.use('/api/items', require('./router/items'));
 // backend/server.js - Add this line
 app.use('/api/stats', require('./router/stats'));
 // MongoDB Connection
-// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log('MongoDB connected'))
-//   .catch(err => console.log(err));
-
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
+  .catch(err => console.log(err));
+
+// mongoose.connect(process.env.MONGODB_URI)
+//   .then(() => console.log('MongoDB connected'))
+//   .catch(err => console.error(err));
 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get("/", (req, res) => {
+  res.send("APP is running ");
+});
