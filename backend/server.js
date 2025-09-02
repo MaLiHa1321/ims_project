f7aff3bf5db1545d6058827adfe46b69ea165f8e
Bushrakhan15 <ummehabiba.bushra2000@gmail.com>
2025-09-02 20:24:28 +0600

    cors are added

--- backend/server.js
+++ backend/server.js
@@ -25,9 +25,14 @@ app.use('/api/items', require('./router/items'));
 // backend/server.js - Add this line
 app.use('/api/stats', require('./router/stats'));
 // MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => console.log('MongoDB connected'))
+//   .catch(err => console.log(err));
+
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+  .catch(err => console.error(err));
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
